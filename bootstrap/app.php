a4b704b68df734c9f53230b582bff67c5ffc5a9e
student-enrollment-system <student-enrollment-system@users.noreply.github.com>
2026-05-23 18:10:52 +0800

    Temporarily remove custom middleware to debug 500 error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,24 +11,27 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // COMMENT OUT ALL CUSTOM MIDDLEWARE FOR NOW
+        // Once the app works, uncomment one by one to find the broken class.
+
         // Global middleware
-        $middleware->append([
-            \App\Http\Middleware\ReportExceptionMW::class,
-            \App\Http\Middleware\PromotionMW::class,
-        ]);
+        // $middleware->append([
+        //     \App\Http\Middleware\ReportExceptionMW::class,
+        //     \App\Http\Middleware\PromotionMW::class,
+        // ]);
 
         // Middleware groups
-        $middleware->group('group_middleware', [
-            \App\Http\Middleware\MiddleWareOne::class,
-            \App\Http\Middleware\MiddleWareTwo::class,
-            \App\Http\Middleware\DownForMaintenanceMW::class,
-        ]);
+        // $middleware->group('group_middleware', [
+        //     \App\Http\Middleware\MiddleWareOne::class,
+        //     \App\Http\Middleware\MiddleWareTwo::class,
+        //     \App\Http\Middleware\DownForMaintenanceMW::class,
+        // ]);
 
         // Route middleware aliases
-        $middleware->alias([
-            'maintenance' => \App\Http\Middleware\DownForMaintenanceMW::class,
-            'sessionUserAccount' => \App\Http\Middleware\SessionUserAccountMW::class,
-        ]);
+        // $middleware->alias([
+        //     'maintenance' => \App\Http\Middleware\DownForMaintenanceMW::class,
+        //     'sessionUserAccount' => \App\Http\Middleware\SessionUserAccountMW::class,
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
