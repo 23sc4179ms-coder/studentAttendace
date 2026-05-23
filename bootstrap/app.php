b4deb4950235c3b77de0479fc7aae7773bda8fd9
student-enrollment-system <student-enrollment-system@users.noreply.github.com>
2026-05-23 16:31:41 +0800

    Fix middleware alias registration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
-
+            'maintenance' => \App\Http\Middleware\DownForMaintenanceMW::class,
+            'sessionUserAccount' => \App\Http\Middleware\SessionUserAccountMW::class,
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
